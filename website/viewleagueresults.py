b01d6ce1ff0dee50daa7b80229e02bc5ed8b2946
Gajo Petrovic <gajop@uns.ac.rs>
2010-10-06 00:31:49 +0200

    improved speed on quering of league matches a bit

--- website/viewleagueresults.py
+++ website/viewleagueresults.py
@@ -32,6 +32,17 @@ sqlalchemysetup.setup()
 
 loginhelper.processCookie()
 
+class AIStats:
+   def __init__(self, name, version):
+      self.name = name
+      self.version = version
+      self.wins = 0
+      self.losses = 0
+      self.draws = 0
+      self.timeouts = 0
+      self.crashes = 0
+      self.score = 0
+      self.games = 0
 
 def go():
    leaguenames = listhelper.tuplelisttolist( sqlalchemysetup.session.query( League.league_name ) )
@@ -55,11 +66,50 @@ def go():
 
    matches = sqlalchemysetup.session.query(LeagueMatch).filter(LeagueMatch.league_id == league.league_id)
    matchids = [match.match_id for match in matches]
-   [success, matchrequestqueue] = gridclienthelper.getproxy().getmatchrequestqueuev1()
-   [success, matchresults] = gridclienthelper.getproxy().getmatchresultsv1()
+   [success, matchrequestqueue] = gridclienthelper.getproxy().getmatchesv1(matchids) #only ids that exist
+   matchresults = filter(lambda x: x['matchresult'][0] == True, matchrequestqueue) #only those that have results
+   for x in matchresults:
+      x['matchresult'] = x['matchresult'][1]
+      x['botrunner_name'] = x['botrunner_name'][1]
+
+   aistats = {}
+   for ai in ais:
+      aistats[(ai.ai_name, ai.ai_version)] = AIStats(ai.ai_name, ai.ai_version)
+
+   for matchresult in matchresults:
+      first = True
+      for ai in matchresult['ais']:
+         aistat = aistats[ai['ai_name'], ai['ai_version']]
+         aistat.games += 1
+         if matchresult['matchresult'] == 'draw':
+            aistat.draws += 1
+            aistat.score += 1
+         elif matchresult['matchresult'] == 'crashed':
+            aistat.crashes += 1
+            aistat.score += 1
+         elif matchresult['matchresult'] == 'gametimeout':
+            aistat.timeouts += 1
+            aistat.score += 1
+         elif matchresult['matchresult'] == 'ai0won':
+            if first:
+               aistat.wins += 1
+               aistat.score += 3
+            else:
+               aistat.losses += 1
+         elif matchresult['matchresult'] == 'ai1won':
+            if first:
+               aistat.losses += 1
+            else:
+               aistat.wins += 1
+               aistat.score += 3
+         first = False 
+   aistats = sorted(aistats.itervalues(), key=lambda x: -x.score)
+
+#   [success, matchrequestqueue] = gridclienthelper.getproxy().getmatchrequestqueuev1()
+#   [success, matchresults] = gridclienthelper.getproxy().getmatchresultsv1()
    [success, mapok ] = gridclienthelper.getproxy().mapexists( league.map_name )
-   matchrequestqueue = [i for i in matchrequestqueue if i['matchrequest_id'] in matchids]
-   matchresults = [i for i in matchresults if i['matchrequest_id'] in matchids]
+#   matchrequestqueue = [i for i in matchrequestqueue if i['matchrequest_id'] in matchids]
+#   matchresults = [i for i in matchresults if i['matchrequest_id'] in matchids]
    #if not mapok:
    #   jinjahelper.message("League " + leaguename + " uses a currently unavailable map: " + league.map_name)
    #   return
@@ -74,7 +124,7 @@ def go():
 
    showform = loginhelper.isLoggedOn()
 
-   jinjahelper.rendertemplate( 'viewleagueresults.html', aipairqueuedmatchcount = aipairqueuedmatchcount, aipairfinishedcount = aipairfinishedcount, indextoai = indextoai, ais = ais, leaguenames = leaguenames, league = league, numais = len(ais), nummatchesperaipair = league.nummatchesperaipair, mapok = mapok, modok = modok, showform = showform )
+   jinjahelper.rendertemplate( 'viewleagueresults.html', aipairqueuedmatchcount = aipairqueuedmatchcount, aipairfinishedcount = aipairfinishedcount, indextoai = indextoai, ais = ais, leaguenames = leaguenames, league = league, numais = len(ais), nummatchesperaipair = league.nummatchesperaipair, mapok = mapok, modok = modok, showform = showform, aistats=aistats )
 
 go()
 
